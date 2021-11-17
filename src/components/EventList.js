d9dffbfd08d3381cb824537a2a5e0dc384d2aeef
Roushan-2104 <sroushan2104@gmail.com>
2021-11-17 19:51:49 +0530

    Done : )

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,9 +3,10 @@ import styles from './EventList.module.css'
 export default function EventList({ events, handleClick }) {
     return (
       <div className={styles.card_list}>
-        {events.map((event, index) => (
+        {events.map((event) => (
           <div className={styles.card} key={event.id}>
-            <h2>{index} - {event.title}</h2>
+            <h2>{event.title}</h2>
+            <p>{event.location} - {event.date}</p>
             <button onClick={() => handleClick(event.id)}>Delete event</button>
           </div>
         ))}
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,12 +4,14 @@ import {useState, useRef} from 'react'
 export default function Form({addEvent}) {
     const [title, setTitle] = useState('')
     const [date, setDate] = useState('')
+    const [location, setLocation] = useState('Manchester')
     // const title = useRef()
     // const date = useRef()
 
     const resetForm = () => {
         setTitle("")
         setDate("")
+        setLocation('Manchester')
         // title.current.value = ""
         // date.current.value = ""
     }
@@ -20,6 +22,7 @@ export default function Form({addEvent}) {
         const event = {
             title:title,
             date:date,
+            location:location,
             id: Math.floor(Math.random()*10000)
         }
         addEvent(event)
@@ -36,6 +39,7 @@ export default function Form({addEvent}) {
                     // ref={title}
                     onChange={(e) => setTitle(e.target.value)} 
                     value={title}
+                    required
                 />
             </label>
             <label>
@@ -44,8 +48,17 @@ export default function Form({addEvent}) {
                     type = "date" 
                     // ref={date}
                     onChange={(e) => setDate(e.target.value)} 
-                    value={date} 
+                    value={date}
+                    required 
                 />
+            </label>
+            <label>
+                <span>Event Location: </span>
+                <select onChange={(e) => setLocation(e.target.value)} required>
+                    <option value="manchester">Manchester</option>
+                    <option value="london">London</option>
+                    <option value="cardiff">Cardiff</option>
+                </select>
             </label>
             <button>Submit</button>
         </form>
